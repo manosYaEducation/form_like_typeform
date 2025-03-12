543e0339cd81abf712f4037f0269c6e17c516dbc
Mauricio Guerrero <mauricioguerrero14@gmail.com>
2025-03-12 14:25:19 -0300

    Corrección de imagen con tilde y credenciales para local y producción en Database.php

--- Realese03-03-25/Mision_Magallanes/Database.php
+++ Realese03-03-25/Mision_Magallanes/Database.php
@@ -4,6 +4,42 @@ class Database {
     public static $instance = null;
     public $connection;
     public $host = '127.0.0.1';  // localhost
+    public $user = 'alphadocere_mag_sostenible';
+    public $password = 'jUN+_}DFO{Rn';
+    public $database = 'alphadocere_mag_sostenible';
+    public $port = 3307;
+
+    public function __construct(){
+        // Se añade el puerto en la conexión
+        //$this->connection = new mysqli($this->host, $this->user, $this->password, $this->database, $this->port);
+$this->connection = new mysqli($this->host, $this->user,$this->password,$this->database);        
+
+        if($this->connection->connect_error){
+            die("Error de conexión: " . $this->connection->connect_error);
+        }
+        
+    // Establecer la codificaci��n de caracteres a UTF-8
+    $this->connection->set_charset("utf8");
+    }
+
+    public static function getInstance(){
+        if(!self::$instance){
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(){
+        return $this->connection;
+    }
+}
+?>
+
+<!-- // models/Database.php
+class Database {
+    public static $instance = null;
+    public $connection;
+    public $host = '127.0.0.1';  // localhost
     public $user = 'root';
     public $password = '';
     public $database = 'forms';
@@ -28,5 +64,4 @@ class Database {
     public function getConnection(){
         return $this->connection;
     }
-}
-?>
+} -->
